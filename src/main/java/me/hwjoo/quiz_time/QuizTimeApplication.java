58a110da4b8965b0c4bab7701ee105d4777bfade
hwjoo <jhw664089@gmail.com>
2025-02-26 13:02:46 +0900

    init project

---
+++ src/main/java/me/hwjoo/quiz_time/QuizTimeApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.quiz_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/quiz_time/QuizTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.quiz_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
